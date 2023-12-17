7d49a6f93781b76b32b8ab561ee7cfe96d1b0869
Manuel Ventura <67847154+Manuel-AC-Ventura@users.noreply.github.com>
2023-12-17 11:56:30 +0100

    Atualização do código
    Conversão de múltiplos arquivos de uma só vez

--- converter_mobi.sh
+++ converter_mobi.sh
@@ -7,17 +7,38 @@ if ! command -v ebook-convert &> /dev/null; then
 fi
 
 # Verifique se o número correto de argumentos foi fornecido
-if [ $# -ne 2 ]; then
-    echo "Uso: $0 \"arquivo.mobi\" \"arquivo.pdf\""
+if [ $# -ne 1 ]; then
+    echo "Uso: $0 \"caminho/do/diretório\""
     exit 1
 fi
 
-# Realize a conversão de MOBI para PDF
-ebook-convert "$1" "$2"
+# Armazena o caminho do diretório fornecido
+directory="$1"
 
-# Verifique se a conversão foi bem-sucedida
-if [ $? -eq 0 ]; then
-    echo "Conversão concluída com sucesso! O arquivo PDF foi salvo como $2."
-else
-    echo "Ocorreu um erro durante a conversão."
+# Verifica se o diretório existe
+if [ ! -d "$directory" ]; then
+    echo "O diretório não existe."
+    exit 1
 fi
+
+# Converte todos os arquivos .mobi no diretório para .pdf
+for file in "$directory"/*.mobi; do
+    if [ -f "$file" ]; then
+        filename=$(basename -- "$file")
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        pdf_file="$directory/$filename.pdf"
+
+        echo "Convertendo $file para $pdf_file..."
+
+        # Realiza a conversão de MOBI para PDF
+        ebook-convert "$file" "$pdf_file"
+
+        # Verifica se a conversão foi bem-sucedida
+        if [ $? -eq 0 ]; then
+            echo "Conversão de $file concluída com sucesso! Arquivo PDF salvo como $pdf_file."
+        else
+            echo "Erro ao converter $file."
+        fi
+    fi
+done
